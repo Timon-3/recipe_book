3dc68a7c586396a6c342f2ddce2c110070ea3e63
unknown <timonfrey@bluewin.ch>
2023-01-13 13:11:44 +0100

    hello

--- recipe_book_web/recipes/views.py
+++ recipe_book_web/recipes/views.py
@@ -1,6 +1,21 @@
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
 def home(request):
     return HttpResponse("Hello, here my recipe book will be located")
+
+def hello(request, name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+    content = "Hello " + clean_name + "! It's " + formatted_now
+    return HttpResponse(content)
